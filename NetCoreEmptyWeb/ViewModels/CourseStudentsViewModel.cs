28ddd46334a71daea9297f833e936f8d6430afb7
rcapraz <69320156+rcapraz@users.noreply.github.com>
2020-08-08 20:11:07 +0300

    Create CourseStudentsViewModel.cs

---
+++ NetCoreEmptyWeb/ViewModels/CourseStudentsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using NetCoreEmptyWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreEmptyWeb.ViewModels
+{
+    public class CourseStudentsViewModel
+    {
+        public Course Course{ get; set; }
+        public List<Student> Students { get; set; }
+    }
+}
