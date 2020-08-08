18184203e45c6fd50cb7414a5668aede86ca7034
rcapraz <69320156+rcapraz@users.noreply.github.com>
2020-08-08 20:09:23 +0300

    Create Student.cs

---
+++ NetCoreEmptyWeb/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreEmptyWeb.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+       
+    }
+}
