9f2ff606526f6eb8c62f658c9a6126133196f665
rcapraz <69320156+rcapraz@users.noreply.github.com>
2020-08-08 20:11:05 +0300

    Create MenuViewComponent.cs

---
+++ NetCoreEmptyWeb/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreEmptyWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreEmptyWeb.ViewComponents
+{
+    public class MenuViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var categories = new List<Category>()
+            {
+                new Category(){Name="Kategori1"},
+                new Category(){Name="Kategori2"},
+                new Category(){Name="Kategori3"}
+            };
+            return View(categories);
+        }
+    }
+}
