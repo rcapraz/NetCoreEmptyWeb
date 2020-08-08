23005b90f14688e0cae419c9d7059bd2d8510e1c
rcapraz <69320156+rcapraz@users.noreply.github.com>
2020-08-08 20:11:10 +0300

    Create ProductsCategoriesViewModel.cs

---
+++ NetCoreEmptyWeb/ViewModels/ProductsCategoriesViewModel.cs
@@ -0,0 +1,14 @@
+﻿//using NetCoreEmptyWeb.Models;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+//namespace NetCoreEmptyWeb.ViewModels
+//{
+//    public class ProductsCategoriesViewModel
+//    {
+//        public List<Category> Categories { get; set; }
+//        public List<Product> Products { get; set; }
+//    }
+//}
