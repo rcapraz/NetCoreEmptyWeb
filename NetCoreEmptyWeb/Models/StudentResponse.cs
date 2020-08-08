0d10bdaa140d8be2560772047b93c9c0337f4106
rcapraz <69320156+rcapraz@users.noreply.github.com>
2020-08-08 20:10:41 +0300

    Create StudentResponse.cs

---
+++ NetCoreEmptyWeb/Models/StudentResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreEmptyWeb.Models
+{
+    public class StudentResponse
+    {
+        [Required(ErrorMessage ="İsminizi giriniz")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Email Giriniz")]
+        [EmailAddress(ErrorMessage ="Mail Adresiniz Uygun Değildir.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Telefon No Giriniz")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Katılma Durumunuzu Bildiriniz")]
+        public bool? Confirm { get; set; }//true,false eğer null ise confirm = null gelir 
+    }
+}
