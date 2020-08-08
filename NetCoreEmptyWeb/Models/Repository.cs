347ea86cb90daef14b3066a94f9f5211ba823997
rcapraz <69320156+rcapraz@users.noreply.github.com>
2020-08-08 20:09:21 +0300

    Create Repository.cs

---
+++ NetCoreEmptyWeb/Models/Repository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreEmptyWeb.Models
+{
+    public class Repository
+    {
+        private static List<StudentResponse> _students = new List<StudentResponse>(); //bir liste oluşturup ismine _students verdik
+        public static List<StudentResponse> Students //başka bir liste oluşturup ismine Students verdik
+        {
+            get
+            {
+                return _students; //bu listenin _students listesini göndermesini istedik
+            }
+        }
+        public static void AddStudent(StudentResponse student) //yeni öğrenci ekleme
+        {
+            _students.Add(student);
+        }
+    }
+}
