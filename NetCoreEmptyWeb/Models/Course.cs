fe6be9476935c58ba674695e885f1d21e015cff9
rcapraz <69320156+rcapraz@users.noreply.github.com>
2020-08-08 20:09:16 +0300

    Create Course.cs

---
+++ NetCoreEmptyWeb/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreEmptyWeb.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        //-------3.6 öncesi--------------------
+        //public string Description { get; set; }
+        //public bool isPublished { get; set; }
+
+    }
+}
